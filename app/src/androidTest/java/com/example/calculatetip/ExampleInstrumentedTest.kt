c3b313e6f923cc6702c19018f7ea8bb357e485fc
Flamel <flamelmamel@gmail.com>
2022-12-18 15:51:45 +0600

    gg

--- app/src/androidTest/java/com/example/calculatetip/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calculatetip/ExampleInstrumentedTest.kt
@@ -1,24 +1,32 @@
 package com.example.calculatetip
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.hamcrest.Matchers.containsString
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class CalculatorTests {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.calculatetip", appContext.packageName)
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.costService))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.calculateButton)).perform(click())
+
+        onView(withId(R.id.tipAmount))
+            .check(matches(withText(containsString("$10.00"))))
     }
 }
